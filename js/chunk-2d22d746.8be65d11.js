7f843c30280607dbf665cadcfb27a17e7c9bb811
Gabriela Neri <gabrielapneri@gmail.com>
2019-06-25 13:31:41 -0300

    build

--- js/chunk-2d22d746.8be65d11.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},u=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{attrs:{id:"about"}},[n("h1",[t._v("About page")])])}],c={name:"about"},r=c,o=n("2877"),s=Object(o["a"])(r,a,u,!1,null,"9c9a5da6",null);e["default"]=s.exports}}]);
-//# sourceMappingURL=chunk-2d22d746.8be65d11.js.map
